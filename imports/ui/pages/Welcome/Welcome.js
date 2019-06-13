80fcda3822848d64cd8957ac8b1dd18c8696b4b4
Timmy <timgabrielnguyen@gmail.com>
2019-06-13 16:29:26 -0700

    Styled Welcome Content with Grid

--- imports/ui/pages/Welcome/Welcome.js
+++ imports/ui/pages/Welcome/Welcome.js
@@ -3,11 +3,25 @@ import AccountsUI from "../../components/AccountsUI";
 import styles from "./styles";
 import {withStyles} from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import {Grid, Typography} from "@material-ui/core";
 
 const Welcome = ({classes}) => {
   return (
     <div className={classes.form}>
-      <AccountsUI />
+      <Grid
+        container
+        className={classes.root}
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid className={classes.content} item xs={12} sm={12} md={6}>
+          <Typography className={classes.header}>
+            Welcome to Team-Mates! 
+          </Typography>
+          <AccountsUI />
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- imports/ui/pages/Welcome/styles.js
+++ imports/ui/pages/Welcome/styles.js
@@ -1,3 +1,15 @@
-const styles = () => ({});
+const styles = () => ({
+  root: {
+    flexGrow: 1
+  },
+  content: {
+    paddingTop: "10%"
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 25,
+    color: "#CC3333"
+  }
+});
 
 export default styles;
